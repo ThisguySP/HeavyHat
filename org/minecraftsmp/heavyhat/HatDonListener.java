876405980a7f80458584f4b891ed19c0df247d88
ThisguySP <eliaslane2@gmail.com>
2013-06-19 02:04:35 -0400

    It *almost* works

--- org/minecraftsmp/heavyhat/HatDonListener.java
+++ org/minecraftsmp/heavyhat/HatDonListener.java
@@ -2,12 +2,56 @@ package org.minecraftsmp.heavyhat;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+import org.bukkit.entity.Player;
+import org.bukkit.entity.HumanEntity;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class HatDonListener implements Listener {
+	private HeavyHat plugin;
+	
+	public HatDonListener(HeavyHat plugin) {
+		this.plugin = plugin;
+	}
+	
+	public void releaseListeners() {
+		HandlerList.unregisterAll(this);
+	}
+	
+	public boolean isArmorEmpty(PlayerInventory inv) {
+		for (int i = 0; i < inv.getArmorContents().length; i++) {
+			if (inv.getArmorContents()[i].getTypeId() != 0) {return false;}
+		}
+		return true;
+	}
 	
 	@EventHandler
 	public void onPlayerInventoryClick(InventoryClickEvent event) {
+		if (event.getWhoClicked() instanceof Player) {
+			plugin.dnote("Instance of Player");
+			if (isArmorEmpty(event.getWhoClicked().getInventory())) {
+				plugin.dnote("Armor slot(s) empty");
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
+		if (!(
+			isArmorEmpty(event.getPlayer().getInventory())
+		)) {
+			event.setCancelled(true);
+			event.getPlayer().setAllowFlight(false);
+			plugin.note(event.getPlayer().getName() + " tried to fly in armor.");
+		}
 	}
 }
--- org/minecraftsmp/heavyhat/HeavyHat.java
+++ org/minecraftsmp/heavyhat/HeavyHat.java
@@ -4,14 +4,34 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import org.minecraftsmp.heavyhat.HatDonListener;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public final class HeavyHat extends JavaPlugin {
-	
+	private boolean d;
 	private HatDonListener HatDonListener;
+	
 	public void onEnable() {
-		HatDonListener = new HatDonListener();
+		HatDonListener = new HatDonListener(this);
 		getServer().getPluginManager().registerEvents(HatDonListener, this);
+		if (!Files.exists(Paths.get("plugins/HeavyHat/config.yml"))) {
+			this.saveDefaultConfig();
+		}
+		d = this.getConfig().getBoolean("debug");
+		dnote("Enabled in debug mode.");
 	}
 	
 	public void onDisable() {
+		HatDonListener.releaseListeners();
+	}
+	
+	
+	
+	public void note(String message) {
+		getLogger().info(message);
+	}
+	
+	public void dnote(String message) {
+		if (d) {getLogger().info(message);}
 	}
 }
